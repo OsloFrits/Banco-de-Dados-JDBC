8c35e36f1f93f37903726be6883cb1f9f76a955f
Eduardo H. Cardoso <eduardofabulam11@gmail.com>
2023-07-25 17:40:33 -0300

    Recuperção de informações do DB e novos metodos usados em DB

--- src/main.java
+++ src/main.java
@@ -1,12 +1,10 @@
 import Indentidades.Conquista;
 import DAO.ConqDAO;
-import db.DB;
-
-import java.sql.Connection;
 import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        /*Scanner sc = new Scanner(System.in);
         String nome, descricao;
         int resp;
         do {
@@ -14,6 +12,7 @@ public class main {
             System.out.println("Nome da conquista e dps uma pequena descrição: ");
             nome = sc.nextLine();
             descricao = sc.nextLine();
+            int relacao = sc.nextInt();
             System.out.println("quer cadastrar este item ??");
             resp = sc.nextInt();
             sc.nextLine();
@@ -21,8 +20,10 @@ public class main {
                 Conquista conq = new Conquista();
                 conq.setNome(nome);
                 conq.setDescri(descricao);
+                conq.setIDrelacao(relacao);
                 new ConqDAO().cadastrarConq(conq);
             }
-        } while (resp != 0);
+        } while (resp != 0);*/
+        System.out.println(ConqDAO.recuperarConq());
     }
 }
